61c04a15ba18228e943a53f2bcdac1e17bd51c3b
rogulski <131556841+rogulskiw@users.noreply.github.com>
2024-12-12 23:50:14 +0100

    Update functions_basic.js

--- functions_basic.js
+++ functions_basic.js
@@ -33,3 +33,15 @@ const weight = 75; // in kg
 const bmiResult = calculateBMI(height, weight);
 console.log("Twoj wskaznik BMI wynosi: " + bmiResult.toFixed(2));
 
+//CALCULATING VAT TAX
+
+function calculateTax(value, taxRate = 0.23){ //default value which can be overwitten
+    const tax = value * taxRate;
+    return tax;
+}
+ 
+ console.log(calculateTax(100)) // basic counting
+ console.log(calculateTax(100, 0.15)) //overwriting default value of taxRate
+
+ 
+
