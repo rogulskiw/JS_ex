0b1499bf640aba9b34d64afe1a5274180464d3a1
rogulski <131556841+rogulskiw@users.noreply.github.com>
2024-12-14 22:24:03 +0100

    Update Array_basic_methods.js

--- Array_basic_methods.js
+++ Array_basic_methods.js
@@ -63,4 +63,53 @@ console.log('Index of first 15: ' + liczby.indexOf(15)) //it shows te first situ
 console.log('Index of last 15: ' + liczby.lastIndexOf(15)) //it shows te last situation where given number appears
 
 
+//FIND
+const library = [
+    { title: "Harry Potter and the Philosopher's Stone", author: "J.K. Rowling",year: 1997},
+    { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960},
+    { title: "Fantastic Beasts and Where to Find Them", author: "J.K. Rowling", year: 2001},
+    { title: "The Lord of the Rings", author: "J.R.R. Tolkien", year: 1994},
+    { title: "Go Set a Watchman", author: "Harper Lee", year: 2015},
+];
+
+//Step 1
+const title = "Fantastic Beasts and Where to Find Them";
+
+const foundBook = library.find((book) => book.title === title);
+if(foundBook) {
+    console.log("Step 1:");
+    console.log("Title:", foundBook.title);
+    console.log("Author:", foundBook.author);
+    console.log("Published in:", foundBook.year);
+} else {
+    console.log("Step 1:");
+    console.log("The book with given title has been found.");
+}
+
+//Step 2
+const author = "Harper Lee"
+const index = library.findIndex((book) => book.author === author);
+if(index !==-1){
+       console.log("Step 2:");
+        console.log("Book's index:", index);
+    } else {
+        console.log("Step 2:");
+        console.log("The book with given author is missing.");
+    }
+    
+//Step 3 
+    const lastIndex = library.reduceRight((acc, book, index) => {
+        if (book.author === author && acc === -1) {
+        return index;
+     }
+        return acc;
+     }, -1);
+
+if (lastIndex !== -1) {
+console.log("Step 3:");
+console.log("Last book index: ", lastIndeks);
+} else {
+console.log("Step 3:");
+console.log("The book with given author is missing.");
+}
      
