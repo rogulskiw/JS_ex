a7be590d0a639125ccb5fad57aef8ea8c79b4346
rogulski <131556841+rogulskiw@users.noreply.github.com>
2024-12-14 19:15:27 +0100

    Update Array_basic_methods.js

--- Array_basic_methods.js
+++ Array_basic_methods.js
@@ -1,5 +1,4 @@
 //ARRAY.AT
-
 const myArray = ['jablko', 'banan', 'cytryna', 'pomarancza'];
 
 function accessArrayElements(index) {
@@ -9,4 +8,26 @@ function accessArrayElements(index) {
 console.log(accessArrayElements(0));
 console.log(accessArrayElements(2));
 console.log(accessArrayElements(3));
+
+//PUSH() ANS POP()
+let shoppingList = [];
+shoppingList.push('Apples');
+shoppingList.push('Milk');
+shoppingList.push('Bread');
+shoppingList.push('Joghurt');
+shoppingList.push('Cheese');
+
+console.log(`Shopping list: ${shoppingList}`);
+shoppingList.pop(shoppingList.length - 1);
+console.log(`Updated shopping list: ${shoppingList}`);
+
+//SHIFT & UNSHIFT
+const schedule = [];
+schedule.unshift("Meeting with the client")
+schedule.unshift("Project presentation")
+schedule.unshift("Stand-up event")
+console.log(schedule)
+schedule.shift();
+console.log(schedule)
+
 
