470406765a22229dd31a955153d4ead59ac66392
rogulski <131556841+rogulskiw@users.noreply.github.com>
2024-12-15 14:44:22 +0100

    Update Array_basic_methods.js

--- Array_basic_methods.js
+++ Array_basic_methods.js
@@ -143,5 +143,53 @@ phone.forEach((product) => {
 console.log("[Name]", product.nazwa, "[Price]", product.cena);
 });
 
+//MAP & REDUCE
+
+const zamowienia = [
+    {
+        klient: "Jan Kowalski",
+        produkty: [
+            { nazwa: "Telefon", cena: 1500 },
+            { nazwa: "Klawiatura", cena: 200 },
+            { nazwa: "Myszka", cena: 50 },
+        ],
+    },
+    {
+        klient: "Anna Nowak",
+        produkty: [
+            { nazwa: "Laptop", cena: 2500 },
+            { nazwa: "Monitor", cena: 1000 },
+        ],
+    },
+    {
+        klient: "Tomasz Krakowiak",
+        produkty: [
+            { nazwa: "Karta graficzna", cena: 2100 },
+            { nazwa: "Procesor", cena: 1500 },
+            { nazwa: "Pamiec RAM", cena: 800 },
+        ],
+    },
+];
+
+
+const wartosciZamowien = zamowienia.map((zamowienie) => { //map creates new array with values
+const wartosc = zamowienie.produkty.reduce(  //reduce takes an array and cummulate it to one value
+    (suma, produkt) => suma + produkt.cena,
+    0
+);
+return {
+    klient: zamowienie.klient,
+    wartoscZamowienia: wartosc,
+};
+});
+
+wartosciZamowien.forEach((zamowienie) => {
+console.log(
+    "[Klient]",
+    zamowienie.klient,
+    "[Wartosc zamowienia]",
+    zamowienie.wartoscZamowienia
+);
+});
 
 
