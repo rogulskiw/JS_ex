f93cb76d9e738a0d6db77c56211490a4dd0ffda0
rogulski <131556841+rogulskiw@users.noreply.github.com>
2024-12-16 21:50:23 +0100

    Update spreadEx.js

--- spreadEx.js
+++ spreadEx.js
@@ -1,3 +1,5 @@
+//Spread
+
 function calculateAverage(...numbers){
     let sum = 0; 
     for (let el of numbers) {
@@ -9,4 +11,29 @@ function calculateAverage(...numbers){
      let result = calculateAverage(4, 8, 6, 2, 10);
     console.log("Srednia liczb:", result);
 
-    
+//...REST
+    const products = ["Coffee", "Tea", "Orange"];
+ 
+    function addProduct(...newProducts) {
+        products.push(...newProducts);
+    }
+ 
+    addProduct("Milk", "Bread", "Chocolate");
+ 
+    console.log("[All products]");
+    products.forEach((element) => {
+        console.log(element);
+    });
+ 
+    function deleteProducts(...indexes) {
+        indexes.forEach((index) => {
+            products.splice(index, 1);
+        });
+    }
+ 
+    deleteProducts(1, 2);
+ 
+    console.log("[Products after deleting]");
+    products.forEach((element) => {
+        console.log(element);
+    });
