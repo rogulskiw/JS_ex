6b64f750163c88b883a54e55de52b53934cfcf2e
rogulski <131556841+rogulskiw@users.noreply.github.com>
2024-12-16 22:05:54 +0100

    Update set.js

--- map/set.js
+++ map/set.js
@@ -24,3 +24,46 @@ magazyn.forEach((ilosc, produkt) => {
     console.log(produkt + ": " + ilosc);
 });
 
+const orders = new Map();
+const order1 = {
+   id: "order001",
+   customerName: "John Doe",
+   items: ["Shirt", "Pants", "Shoes"],
+   };
+const order2 = {
+   id: "order002",
+   customerName: "Jane Smith",
+   items: ["Dress", "Hat"],
+   };
+   
+   orders.set(order1.id, order1)
+   orders.set(order2.id, order2)
+   
+   console.log(`Liczba zamowien w systemie: ${orders.size}`);
+   console.log("Zamowienia w systemie:");
+  
+   for(const [orderId, order] of orders.entries()){
+       console.log(`[ID zamowienia: ${orderId}]`);
+       console.log(`Klient: ${order.customerName}`);
+       console.log(`Przedmioty: ${order.items.join(", ")}`);
+   }
+   
+   const orderIdToCheck = "order001";
+   if(orders.has("order001")){
+       console.log("Zamowienie o ID order001 istnieje w systemie.")
+   } else {
+       console.log("Zamowienie o ID order001 nie istnieje w systemie.")
+   }
+   
+   const orderIdToGet = "order002";
+   const orderToGet = orders.get(orderIdToGet);
+   if (orderToGet) {
+       console.log("[Informacje o zamowieniu:]");
+       console.log(`ID zamowienia: ${orderToGet.id}`);
+       console.log(`Klient: ${orderToGet.customerName}`);
+       console.log(`Przedmioty: ${orderToGet.items.join(", ")}`);
+   } else {
+       console.log(`Nie znaleziono zamowienia o ID ${orderIdToGet}.`);
+   }
+   
+   
