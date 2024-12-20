c404b3ac672cf86dc67b6158047f20dd072f7f57
rogulski <131556841+rogulskiw@users.noreply.github.com>
2024-12-20 22:26:40 +0100

    Update dateJS.js

--- dateJS.js
+++ dateJS.js
@@ -15,3 +15,20 @@ function countDaysOfWeek(){
    });
 }
 
+//DISPLAYING CURRENT DATE AND TIME
+
+function displayCurrentDateTime(){
+    let currentDate = new Date();
+   let year = currentDate.getFullYear();
+   let month = currentDate.getMonth();
+   let day = currentDate.getDate();
+   let hours = currentDate.getHours();
+   let minutes = currentDate.getMinutes();
+   let seconds = currentDate.getSeconds();
+   let milliseconds = currentDate.getMilliseconds();
+   
+   console.log(`Current Date: ${day}/${month + 1}/${year}`);
+   console.log(`Current Time: ${hours}:${minutes}:${seconds}.${milliseconds}`);
+}
+displayCurrentDateTime();
+
