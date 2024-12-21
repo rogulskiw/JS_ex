1f7d078fa954bde2f5e99c54cf17355f2433885c
rogulski <131556841+rogulskiw@users.noreply.github.com>
2024-12-21 22:36:55 +0100

    Update JSON.js

--- JSON.js
+++ JSON.js
@@ -17,3 +17,14 @@ convertToJSON(person);
 
 //JSON.parse()
 
+const jsonUser = '{"name": "John Doe", "age": 30, "email": "john@example.com"}';
+function parseUser(user1) {
+    const user = JSON.parse(user1);
+    return user;
+}
+
+const user = parseUser(jsonUser);
+
+console.log("Name:", user.name);
+console.log("Age:", user.age)
+console.log("Email:", user.email)
