8a6b543b5f3b757e83417636f1ba438dfdbcf16a
rogulski <131556841+rogulskiw@users.noreply.github.com>
2024-12-23 11:09:24 +0100

    Update timeout_interval.js

--- timeout_interval.js
+++ timeout_interval.js
@@ -40,3 +40,32 @@ function newYearCounting() {
 
 newYearCounting();
 
+//ROUTE OF THE CAR
+
+const route = [
+    { x: 0, y: 0 },
+    { x: 100, y: 50 },
+    { x: 200, y: 100 },
+    { x: 300, y: 150 },
+    { x: 400, y: 200 },
+];
+
+const car = {
+    position: 0,
+    velocity: 10,
+};
+
+function moveCar(){
+    if (car.position >= route.length) {
+        console.log('The trip is over!');
+        clearInterval(intervalId);
+        return;
+        }
+
+ const pitStop = route[car.position];
+ console.log(`The car is in position x: ${pitStop.x}, y: ${pitStop.y}`);
+ 
+car.position++;
+}
+const intervalId = setInterval(moveCar, 1000);
+
