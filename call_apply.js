4e8d5254ab0c83760bf8e979152086a2e2e1d8b4
rogulski <131556841+rogulskiw@users.noreply.github.com>
2024-12-24 14:17:02 +0100

    Update call_apply.js

--- call_apply.js
+++ call_apply.js
@@ -25,4 +25,29 @@ console.log(`Multiplication result: ${calculator.result}`)
 calculator.result = subtract.call(calculator,calculator.x,calculator.y);
 console.log(`Subtraction result: ${calculator.result}`)
 
-//
+//APPLY
+
+function findMax(numbers) {
+    return Math.max.apply(null, numbers);
+}
+
+function findMin(numbers) {
+    return Math.min.apply(null, numbers);
+}
+
+function calculateAverage(numbers) {
+    const sum = numbers.reduce((acc, curr) => acc + curr, 0);
+    return sum / numbers.length;
+}
+
+const numbers = [12, 6, 8, 17, 3, 10];
+
+const max = findMax.apply(null, [numbers]);
+console.log("The highest number is:", max);
+
+const min = findMin.apply(null, [numbers]);
+console.log("The lowest number is", min);
+
+const average = calculateAverage.apply(null, [numbers]);
+console.log("Average:", Number(average.toFixed(2)));
+
