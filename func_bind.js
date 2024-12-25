ad54475e7c452b66ebca2c802fa17eff218aeda0
rogulski <131556841+rogulskiw@users.noreply.github.com>
2024-12-25 23:25:23 +0100

    Update func_bind.js

--- func_bind.js
+++ func_bind.js
@@ -1,3 +1,4 @@
+//BIND INTRODUCTION
 function calculateDiscount(percent){
     return this.price * (percent/100);
 }
@@ -11,3 +12,34 @@ let calculateProductDiscount = calculateDiscount.bind(product);
 const discount = calculateProductDiscount(20);
 
 console.log("Discount:", discount)
+
+//SMALL ONLINE SHOP
+
+function calculateTotal() {
+    return this.unitPrices.reduce(
+        (acc, curr, index) => acc + curr * this.quantities[index],
+        0
+    );
+}
+
+function displayOrderDetails() {
+    console.log("Order details:");
+    this.products.forEach((product, index) => {
+        console.log(
+            `${product} - quantity: ${this.quantities[index]}, price: ${this.unitPrices[index]}`
+        );
+    });
+    const total = calculateTotal.call(this);
+    console.log("Overall order cost:", total);
+}
+
+const order = {
+    products: ["Shirt", "Trousers", "Shoes"],
+    quantities: [2, 1, 1],
+    unitPrices: [50, 80, 120],
+};
+
+const displayOrderDetailsForOrder = displayOrderDetails.bind(order);
+
+displayOrderDetailsForOrder();
+
