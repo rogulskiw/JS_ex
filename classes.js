a53c3c28b7ab10aabd2a6f8bfd3bfbe7dff1d6e5
rogulski <131556841+rogulskiw@users.noreply.github.com>
2024-12-28 19:04:30 +0100

    Update classes.js

--- classes.js
+++ classes.js
@@ -39,3 +39,44 @@ console.log(myBook.getAuthor());
 console.log(myBook.getDescription());
 
 myBook.printDetails();
+
+//TV IN CLASS
+
+class TV {
+    constructor(brand){
+        this.brand = brand;
+        this.on = false; 
+        this.volume = 50;
+        this.channel= 1;
+    }
+   switchOn(){
+       this.on= true;
+       console.log('TV has been switched on.')
+   }
+   
+   switchOff(){
+       this.on = false
+       console.log('TV has been switched off.')
+   }
+   
+   changeVolume(volume){
+       if(volume > 0 && volume <=100){
+           this.volume = volume;
+       }
+       console.log("Volume is now on level",glosnosc)
+    }
+    
+    changeChannel(channel){
+        this.channel = channel;
+        console.log("Channel has been changed to", kanal)
+    }
+    
+}
+
+const myTv = new TV();
+mojTelewizor.switchOn();
+mojTelewizor.changeVolume(75);
+mojTelewizor.changeChannel(5);
+mojTelewizor.switchOff();
+
+
