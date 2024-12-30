a7d96c5d201e0a8d0630a68eeda0387b8b9a63cb
rogulski <131556841+rogulskiw@users.noreply.github.com>
2024-12-31 00:03:56 +0100

    Update privateProperties.js

--- privateProperties.js
+++ privateProperties.js
@@ -55,3 +55,53 @@ account1.deposit(200);
 account1.withdraw(100);
 
 console.log(account1.getBalance());
+
+//TASKS LIST
+
+class TaskList {
+    #tasks;
+
+    constructor() {
+        this.#tasks = [];
+    }
+
+    addTask(title) {
+        const task = {
+            title: title,
+            status: "niezakonczone",
+        };
+        this.#tasks.push(task);
+    }
+
+    removeTask(title) {
+        const index = this.#tasks.findIndex((task) => task.title === title);
+        if (index !== -1) {
+            this.#tasks.splice(index, 1);
+        }
+    }
+
+    markTaskAsDone(title) {
+        const task = this.#tasks.find((task) => task.title === title);
+        if (task) {
+            task.status = "zakonczone";
+        }
+    }
+
+    showTasks() {
+        this.#tasks.forEach((task) => {
+            console.log(`Tytul: ${task.title} | Status: ${task.status}`);
+        });
+    }
+}
+
+const myTasks = new TaskList();
+myTasks.addTask("Zadanie 1");
+myTasks.addTask("Zadanie 2");
+myTasks.addTask("Zadanie 3");
+myTasks.showTasks();
+
+myTasks.markTaskAsDone("Zadanie 2");
+myTasks.removeTask("Zadanie 3");
+myTasks.showTasks();
+
+
