f3ebade757f3072c5f5f589f4dabdefe174f66c3
rogulski <131556841+rogulskiw@users.noreply.github.com>
2024-12-30 15:12:19 +0100

    Update privateProperties.js

--- privateProperties.js
+++ privateProperties.js
@@ -21,3 +21,37 @@ console.log(person1.introduce());
 person1.changeAddress("ul. Sloneczna 10");
 console.log(person1.introduce());
 
+//BANK ACCOUNT AND METHODS
+
+class BankAccount{
+    constructor(accountNumber, balance){
+        this._accountNumber = accountNumber;
+        this._balance = balance;
+    }
+    _checkMinimumBalance(amount){
+        const minimumBalance = 100;
+        if(this._balance < minimumBalance){
+            console.log(`Saldo jest zbyt niskie`)
+        }
+    }
+    
+    getBalance(){
+        return this._balance;
+    }
+    
+    deposit(amount){
+        return this._balance +=amount
+    }
+    
+   withdraw(amount) {
+       this._checkMinimumBalance();
+       this._balance -= amount;
+   }
+
+}
+
+const account1 = new BankAccount("1234567890", 500);
+account1.deposit(200);
+account1.withdraw(100);
+
+console.log(account1.getBalance());
