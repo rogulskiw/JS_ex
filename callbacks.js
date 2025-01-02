851b0b67fa71c1c7171dcfac33668f5d10b109e4
rogulski <131556841+rogulskiw@users.noreply.github.com>
2025-01-02 11:39:50 +0100

    Update callbacks.js

--- callbacks.js
+++ callbacks.js
@@ -7,3 +7,37 @@ function fetchData(callback) {
 fetchData((response) => {
     console.log(response);
 });
+
+//MORE CALLBACKS
+
+function getUsersFromDatabase(callback){
+    setTimeout(function() {
+       const users = [
+           { id: 1, name: "Jan Kowalski" },
+           { id: 2, name: "Anna Nowak" },
+           { id: 3, name: "Piotr Wisniewski" },
+           ];
+           callback(users);
+    }, 2000);
+}
+
+
+function getEmail(userId, callback){
+    setTimeout(function(){
+        const email = userId + "@example.com";
+        callback(email);
+    }, 1500);
+}
+
+function mainFunction(){
+    getUsersFromDatabase((users)=>{
+       console.log("Uzytkownicy:", users);
+       users.forEach(function(user){
+           getEmail(user.id, function(email){
+               console.log(`Uzytkownik ${user.name} ma adres e-mail: ${email}`);
+           });
+       });
+   });
+}
+
+mainFunction();
