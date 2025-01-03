44a1a110b915de6ef3907c595a54628f6c336306
rogulski <131556841+rogulskiw@users.noreply.github.com>
2025-01-03 23:31:02 +0100

    Update promises.js

--- promises.js
+++ promises.js
@@ -13,3 +13,68 @@ fetchData.catch((error)=> {
    console.log("Blad podczas pobierania danych:", error);
 });
 
+//Promises chaining
+
+
+function getMenu() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const menu = [
+                { name: "Pizza Margherita", price: 20 },
+                { name: "Spaghetti Bolognese", price: 15 },
+                { name: "Caesar Salad", price: 10 },
+            ];
+            resolve(menu);
+        }, 2000);
+    });
+}
+
+function selectDish(menu, index) {
+    return new Promise((resolve, reject) => {
+        if (index >= 0 && index < menu.length) {
+            resolve(menu[index]);
+        } else {
+            reject("Wrong dish's index");
+        }
+    });
+}
+
+function calculateTotal(selectedDishes) {
+    return new Promise((resolve) => {
+        const total = selectedDishes.reduce(
+            (acc, dish) => acc + dish.price,
+            0
+        );
+        resolve(total);
+    });
+}
+
+function confirmOrder(total) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(
+                "Zamowienie potwierdzone. Laczna kwota: " + total + " zl"
+            );
+        }, 1500);
+    });
+}
+
+getMenu()
+    .then((menu) => {
+        console.log("Menu:", menu);
+        return selectDish(menu, 0);
+    })
+    .then((selectedDish) => {
+        console.log("Wybrane danie:", selectedDish);
+        return calculateTotal([selectedDish]);
+    })
+    .then((total) => {
+        console.log("Laczna cena zamowienia:", total, "zl");
+        return confirmOrder(total);
+    })
+    .then((confirmation) => {
+        console.log(confirmation);
+    })
+    .catch((error) => {
+        console.log("Blad:", error);
+    });
