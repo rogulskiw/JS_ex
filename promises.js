cdf29ac7665d2b15873a5e25a304f2a81c3b0bbc
rogulski <131556841+rogulskiw@users.noreply.github.com>
2025-01-06 00:36:32 +0100

    Update promises.js

--- promises.js
+++ promises.js
@@ -231,3 +231,41 @@ Promise.allSettled([fetchUserData(), fetchOrderData(), fetchProductData()])
 .catch((error)=> {
    console.log("Wystapil blad:", error);
 });
+
+//Promise.race
+
+function getDeliveryTimeFromSupplierA() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(2000);
+        }, 2000);
+    });
+}
+
+function getDeliveryTimeFromSupplierB() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(1500);
+        }, 1500);
+    });
+}
+
+function raceBetweenSuppliers() {
+    Promise.race([
+        getDeliveryTimeFromSupplierA(),
+        getDeliveryTimeFromSupplierB(),
+    ])
+        .then((winner) => {
+            if (winner === 2000) {
+                console.log("Zwyciezca jest dostawca A!");
+            } else if (winner === 1500) {
+                console.log("Zwyciezca jest dostawca B!");
+            }
+        })
+        .catch((error) => {
+            console.error("Wystapil blad:", error);
+        });
+}
+
+raceBetweenSuppliers();
+
