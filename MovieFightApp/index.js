50c02f595fd9d1d9041ac92a48ec0fb9fc39a157
rogulski <131556841+rogulskiw@users.noreply.github.com>
2025-01-09 00:49:46 +0100

    Adding tutorial

--- MovieFightApp/index.js
+++ MovieFightApp/index.js
@@ -1,27 +1,41 @@
-const fetchData = async (searchTerm) => {
-    const response = await axios.get('http://www.omdbapi.com/', {
-     params: {
-        apikey:  'b510411d',
-        s: searchTerm
-        }   
-    }); 
-
-    if(response.data.Error) {
-        return [];
-    }
-
-    return response.data.Search;
-};
-
-createAutoComplete({
-    root: document.querySelector('.autocomplete'),
+const autoCompleteConfig = {
     renderOption(movie){
         const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
         return `
             <img src="${imgSrc}" />
             ${movie.Title}
         `
+    },
+    onOptionSelect(movie){
+        document.querySelector('.tutorial').classList.add('is-hidden');
+        onMovieSelect(movie);
+    }, 
+    inputValue(movie){
+        return movie.Title
+    },
+    async fetchData(searchTerm){
+            const response = await axios.get('http://www.omdbapi.com/', {
+             params: {
+                apikey:  'b510411d',
+                s: searchTerm
+                }   
+            }); 
+        
+            if(response.data.Error) {
+                return [];
+            }
+            return response.data.Search; 
     }
+}
+
+createAutoComplete({
+    ...autoCompleteConfig,
+    root: document.querySelector('#left-autocomplete')
+})
+
+createAutoComplete({
+    ...autoCompleteConfig,
+    root: document.querySelector('#right-autocomplete')
 })
 
 
