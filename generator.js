3f6e5afbd0a53263db22d82ffb29f872e50a52b1
rogulski <131556841+rogulskiw@users.noreply.github.com>
2025-01-10 00:19:31 +0100

    Update generator.js

--- generator.js
+++ generator.js
@@ -10,4 +10,25 @@ for(let i=0; i<5;i++){
     console.log(generator.next().value.toFixed(2));
 }
 
+//PASSWORD GENERATOR
+
+function* productCodeGenerator(length, prefix = ""){
+    let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let codeLength = length - prefix.length;
+    
+    while(true){
+        let code = prefix; 
+        for(let i = 0; i <= codeLength; i++){
+            const randomIndex = Math.floor(Math.random()*characters.length);
+            code += characters.charAt(randomIndex);
+        }
+        yield code;
+    }
+}
+
+let generator1 = productCodeGenerator(8, "PRD");
+console.log(generator.next().value);
+console.log(generator.next().value);
+console.log(generator.next().value);
+
 
