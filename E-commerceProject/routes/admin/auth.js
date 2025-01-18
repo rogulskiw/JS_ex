42475b7a40cf757238022d99f2808bde027cc581
rogulski <131556841+rogulskiw@users.noreply.github.com>
2025-01-19 00:04:40 +0100

    refactoring

--- E-commerceProject/routes/admin/auth.js
+++ E-commerceProject/routes/admin/auth.js
@@ -1,89 +1,66 @@
 const express = require('express');
 const { check, validationResult } = require('express-validator');
 
-const usersRepo = require('../../repositories/users')
-const signupTemplate = require('../../views/admin/auth/signup')
-const signinTemplate = require('../../views/admin/auth/signin')
+const usersRepo = require('../../repositories/users');
+const signupTemplate = require('../../views/admin/auth/signup');
+const signinTemplate = require('../../views/admin/auth/signin');
+const {
+  requireEmail,
+  requirePassword,
+  requirePasswordConfirmation,
+  requireEmailExists,
+  requireValidPasswordForUser
+} = require('./validators');
 
 const router = express.Router();
 
 router.get('/signup', (req, res) => {
-    res.send(signupTemplate({ req }));
-})
-//BODY PARSER WRITTEN BY HAND AND REPLACED BY INJECTED LIBRARY bodyParser.urlencoded
-// const bodyParser = (req, res, next) => {
-//     if (req.method === 'POST'){
-//         req.on('data', data => {
-//             const parsed = data.toString('utf8').split('&');
-//             const formData = {};
-//             for(let pair of parsed){
-//              const [key, value] = pair.split('='); 
-//              formData[key] = value; 
-//             };
-//             req.body = formData;
-//             next();
-//             });
-//     } else {
-//         next();
-//     }
-//  };
+  res.send(signupTemplate({ req }));
+});
 
-router.post('/signup', [
-    check('email').trim().normalizeEmail().isEmail(),
-    check('password').trim().isLength({ min: 4, max:20 }),
-    check('passwordConfirmation').trim().isLength({ min:4, max:20 })
-], 
-async (req, res) => {
+router.post(
+  '/signup',
+  [requireEmail, requirePassword, requirePasswordConfirmation],
+  async (req, res) => {
     const errors = validationResult(req);
-    console.log(errors);
-    
+
+    if (!errors.isEmpty()) {
+      return res.send(signupTemplate({ req, errors }));
+    }
+
     const { email, password, passwordConfirmation } = req.body;
-
-    const existingUser = await usersRepo.getOneBy({ email });
-    if(existingUser) {
-        return res.send('Email in use');
-    }
-
-    if (password !== passwordConfirmation){
-        return res.send('Passwords must match');
-    }
-    //creating a user in our repo 
-    const user = await usersRepo.create( {email: email, password: password});
-    //store the id of that user 
-    req.session.userId = user.id; 
-
-    res.send('Account created!!!');
-});
-
-router.get('/signout', (req, res) => {
-    req.session = null;
-    res.send('You are logged out');
-});
-
-router.get('/signin', (req, res) => {
-    res.send(signinTemplate())
-});
-
-router.post('/signin', async (req, res) => {
-    const { email, password } = req.body;
-
-    const user = await usersRepo.getOneBy({ email: email });
-
-    if (!user) {
-        return res.send('Email not found');
-    }
-
-    const validPassword = await usersRepo.comparePasswords(
-        user.password, 
-        password
-    );
-    if(!validPassword){
-        return res.send('Invalid password');
-    }
+    const user = await usersRepo.create({ email, password });
 
     req.session.userId = user.id;
 
-    res.send('You are signed in!!!')
-})
+    res.send('Account created!!!');
+  }
+);
 
-module.exports = router; //it helps to share this file to the other in a project
+router.get('/signout', (req, res) => {
+  req.session = null;
+  res.send('You are logged out');
+});
+
+router.get('/signin', (req, res) => {
+  res.send(signinTemplate());
+});
+
+router.post(
+  '/signin',
+  [requireEmailExists, requireValidPasswordForUser],
+  async (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
+
+    const { email } = req.body;
+
+    const user = await usersRepo.getOneBy({ email });
+
+    req.session.userId = user.id;
+
+    res.send('You are signed in!!!');
+  }
+);
+
+module.exports = router;
