bd08c48ab8d0753e7d0d997c1bbceec8d1ba4229
rogulski <131556841+rogulskiw@users.noreply.github.com>
2025-01-18 22:22:50 +0100

    added express-validator

--- E-commerceProject/views/admin/auth/signin.js
+++ E-commerceProject/views/admin/auth/signin.js
@@ -1,5 +1,7 @@
+const layout = require('../layout');
+
 module.exports = () => {
-    return `
+    return layout({ content:  `
         <div> 
             <form method="POST">
                 <input name="email" placeholder="email" />
@@ -7,6 +9,6 @@ module.exports = () => {
                 <button>Sign In </button>
             </form>
         </div>
-
-    `
-}
+        `
+    });
+};
