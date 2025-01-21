661d977f0f43e66d81e50b6513953be47e76d02a
rogulski <131556841+rogulskiw@users.noreply.github.com>
2025-01-21 18:04:40 +0100

    Creating middleware

---
+++ E-commerceProject/routes/admin/middlewares.js
@@ -0,0 +1,15 @@
+const { validationResult } = require ('express-validator');
+
+module.exports = {
+    handleErrors(templateFunc) {
+        return (req, res, next) => { //it's middleware function and NEXT is a reference to a function which express is doing and tells it to continue process 
+            const errors = validationResult(req);
+
+            if(!errors.isEmpty()){
+                return res.send(templateFunc({ errors }));
+            }
+            
+            next();
+        };
+    }
+};
